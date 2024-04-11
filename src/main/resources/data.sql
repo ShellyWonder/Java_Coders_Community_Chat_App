8090e9d4028f97d00b6dc0d1892a9fc7c527034e
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-04-11 18:19:48 -0500

    added seed data for channels

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+-- seeding channel info placeholder for now; 
+-- v.2 will have more channels user-created;
+
+INSERT INTO channel (name, description) VALUES ('Java General', 'Whatever is on your mind about Java');
+INSERT INTO channel (name, description) VALUES ('Frontend Frameworks', 'Angular, React, Vue, Svelte, etc. discussions');
+INSERT INTO channel (name, description) VALUES ('Spring Security', 'How to secure your Spring Boot application and more');
