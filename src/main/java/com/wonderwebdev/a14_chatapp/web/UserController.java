fab1d5613fd147b35b1ac8b27942f43b52f9023c
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-04-25 14:05:35 -0500

    correcting model/ thymeleaf issues

--- src/main/java/com/wonderwebdev/a14_chatapp/web/UserController.java
+++ src/main/java/com/wonderwebdev/a14_chatapp/web/UserController.java
@@ -3,12 +3,17 @@ package com.wonderwebdev.a14_chatapp.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.wonderwebdev.a14_chatapp.domain.User;
+
+import org.springframework.ui.Model;
+
 @Controller
-// Note: user login being handled by the AuthController class
+// Note: user login being handled by the AuthController class because it requires a RESTful API
 public class UserController {
     @GetMapping("/")
-	public String home() {
+	public String home(Model model) {
 		System.out.println("Home controller method called");
+		model.addAttribute("user", new User());
 		return "index";
 	}
 }
