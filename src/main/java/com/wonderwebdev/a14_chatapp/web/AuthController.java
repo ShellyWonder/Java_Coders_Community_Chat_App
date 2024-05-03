b20341e332009992c9e220b7b65a784169ab7bd0
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-05-03 17:43:49 -0500

    added requestMapping and constructor to ensure continuity

--- src/main/java/com/wonderwebdev/a14_chatapp/web/AuthController.java
+++ src/main/java/com/wonderwebdev/a14_chatapp/web/AuthController.java
@@ -2,7 +2,6 @@ package com.wonderwebdev.a14_chatapp.web;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,12 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.wonderwebdev.a14_chatapp.domain.User;
 import com.wonderwebdev.a14_chatapp.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @RestController
+@RequestMapping("/api")
 public class AuthController {
-    
-    @Autowired
+        
     private UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/login")
     public ResponseEntity<Map<String, Object>> loginUser(@RequestBody User loginUser) {
