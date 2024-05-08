b81c2ae56fee27fbda753b2d24990b2213152b27
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-05-07 20:44:26 -0500

    removed unused import

--- src/main/java/com/wonderwebdev/a14_chatapp/web/ChannelViewController.java
+++ src/main/java/com/wonderwebdev/a14_chatapp/web/ChannelViewController.java
@@ -1,5 +1,4 @@
 package com.wonderwebdev.a14_chatapp.web;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
