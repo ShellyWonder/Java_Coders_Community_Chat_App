f5a66db313b0a8eb24530d2b206a2519316b0648
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-05-25 17:48:44 -0500

    adding lastName

--- src/main/java/com/wonderwebdev/a14_chatapp/dto/UserDTO.java
+++ src/main/java/com/wonderwebdev/a14_chatapp/dto/UserDTO.java
@@ -43,4 +43,11 @@ public class UserDTO {
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
 }
