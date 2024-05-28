b5ffce57a2cb1429e68b7af0dc682266adaf668e
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-05-27 22:09:37 -0500

    added new method to correct user login issue

--- src/main/java/com/wonderwebdev/a14_chatapp/repository/UserRepository.java
+++ src/main/java/com/wonderwebdev/a14_chatapp/repository/UserRepository.java
@@ -3,10 +3,13 @@ package com.wonderwebdev.a14_chatapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import com.wonderwebdev.a14_chatapp.domain.User;
 
-public interface UserRepository  extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
+    
    @Query("SELECT u FROM User u WHERE u.userName = :userName")
-User findByUserName(@Param("userName") String userName);
+    User findByUserName(@Param("userName") String userName);
+
+    @Query("SELECT u FROM User u WHERE u.userName = :userName AND u.password = :password")
+    User findByUserNameAndPassword(@Param("userName") String userName, @Param("password") String password);
 }
