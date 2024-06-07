f002ea83e4cb6855ea408f06d3426d5f340793f6
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-06-06 19:02:45 -0500

    modified UserRepository

--- src/main/java/com/wonderwebdev/a14_chatapp/repository/UserRepository.java
+++ src/main/java/com/wonderwebdev/a14_chatapp/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.wonderwebdev.a14_chatapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +14,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.userName = :userName AND u.password = :password")
     User findByUserNameAndPassword(@Param("userName") String userName, @Param("password") String password);
+
+   // Find users by channel ID
+   @Query("SELECT u FROM User u JOIN u.channels c WHERE c.id = :channelId")
+   List<User> findUsersByChannelId(@Param("channelId") Long channelId);
 }
