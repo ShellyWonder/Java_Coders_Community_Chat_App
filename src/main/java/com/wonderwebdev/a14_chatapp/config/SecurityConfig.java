4a6499e41ad374bb99656da9442ed54b1c629e58
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-06-09 12:48:40 -0500

    updated SecurityConfig

--- src/main/java/com/wonderwebdev/a14_chatapp/config/SecurityConfig.java
+++ src/main/java/com/wonderwebdev/a14_chatapp/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer; // Add this import
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,17 +16,22 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
          return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(authorizeHttpRequests ->
-                authorizeHttpRequests
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(authorizeRequests ->
+            authorizeRequests
                     .requestMatchers("/api/auth/register", "/api/auth/login", "/public/**").permitAll()
                     .anyRequest().authenticated()
             )
