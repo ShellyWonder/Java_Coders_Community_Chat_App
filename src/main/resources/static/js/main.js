75c5fae3cedf531d02f0b3eba9df9d00864da717
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-06-24 16:37:09 -0500

    refactor to accomodate channel view functionality

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,33 +1,44 @@
 // main.js: Handles the main initialization logic, including attaching event listeners and fetching the initial data.
 
-import { checkLoginStatus, handleLoginStatus, attachAuthEventListeners, attachEventListeners } from './auth.js';
-import { fetchAndUpdateChannels } from './uiUtil.js';
-import { attachChannelEventListeners } from './channel.js';
+import { checkLoginStatus, handleLoginStatus, attachAuthEventListeners } from './auth.js';
+import { fetchAndUpdateChannels, attachJoinChannelEventListener, showOrHideNavDropdown, showOrHideLogoutButton, updateUserNameDisplay } from './uiUtil.js';
+import { initializeChannelPage } from './channel.js';
 
 document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
 
 function onDOMContentLoaded() {
     console.log("DOMContentLoaded event fired");
     const isLoggedIn = checkLoginStatus();
+    console.log("Is logged in:", isLoggedIn);
 
     handleInitialPageLoad(isLoggedIn);
-    attachEventListeners();
-
-    if (document.querySelector(".channel")) {
-        attachChannelEventListeners();
-        console.log("Channel event listeners attached");
-    }
-
+    //used for login and registration forms
+    attachAuthEventListenersIfNeeded();
+    //buttons used for selecting a channel to join
+    attachJoinChannelEventListener();
+    
     // Only call fetchAndUpdateChannels if the user is already logged in before the login action
     if (isLoggedIn && window.location.pathname === "/") {
         fetchAndUpdateChannels();
     }
+    
+    // Attach event listeners and initialize the channel page if on a channel
+    if (window.location.pathname.startsWith("/channel/")) {
+        initializeChannelPage();
+    }
+    
+    //used for presenting channel dropdown list in Navbar
+    showOrHideNavDropdown(isLoggedIn);
+    //used for presenting logout button in Navbar to logged in user
+    showOrHideLogoutButton(isLoggedIn);
+    //used for presenting logged in username in channel selection card 
+    updateUserNameDisplay();
 }
 
 function handleInitialPageLoad(isLoggedIn) {
+    console.log("Handling initial page load, isLoggedIn:", isLoggedIn);
     if (isLoginOrHomePage()) {
         handleLoginStatus(isLoggedIn);
-        attachAuthEventListenersIfNeeded();
     } else if (!isLoggedIn) {
         redirectToLogin();
     }
@@ -46,4 +57,3 @@ function attachAuthEventListenersIfNeeded() {
 function redirectToLogin() {
     window.location.href = "/login";
 }
-
