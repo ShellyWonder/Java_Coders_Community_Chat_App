889af0203a1f0862f682a22a23594550f23e2c16
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-07-17 11:32:33 -0500

    added config file to remove nanoseconds from date/time Jsone serialized output

--- src/main/java/com/wonderwebdev/a14_chatapp/config/JacksonConfig.java
+++ src/main/java/com/wonderwebdev/a14_chatapp/config/JacksonConfig.java
@@ -1,21 +1,32 @@
 package com.wonderwebdev.a14_chatapp.config;
 
+import java.time.LocalDateTime;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class JacksonConfig {
     @Bean
     public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
-        ObjectMapper mapper = builder.createXmlMapper(false).build();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); 
-        return mapper;
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        
+        // Register custom LocalDateTime serializer
+        //Removes nanoseconds from the serialized output
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer());
+        
+        objectMapper.registerModule(module);
+        objectMapper.registerModule(javaTimeModule);
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
     }
     
 }
---
+++ src/main/java/com/wonderwebdev/a14_chatapp/config/LocalDateTimeSerializer.java
@@ -0,0 +1,24 @@
+package com.wonderwebdev.a14_chatapp.config;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeSerializer extends StdSerializer<LocalDateTime> {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public LocalDateTimeSerializer() {
+        super(LocalDateTime.class);
+    }
+
+    @Override
+    public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        gen.writeString(value.format(formatter));
+    }
+}
+
