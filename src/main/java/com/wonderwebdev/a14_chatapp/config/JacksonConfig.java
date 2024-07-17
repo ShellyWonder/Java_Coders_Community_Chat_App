6d386f1117ef97c77dbcc1a7db665c79e9c40b1e
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-07-17 10:21:44 -0500

    added config file to help with date/time json config

---
+++ src/main/java/com/wonderwebdev/a14_chatapp/config/JacksonConfig.java
@@ -0,0 +1,21 @@
+package com.wonderwebdev.a14_chatapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper mapper = builder.createXmlMapper(false).build();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); 
+        return mapper;
+    }
+    
+}
