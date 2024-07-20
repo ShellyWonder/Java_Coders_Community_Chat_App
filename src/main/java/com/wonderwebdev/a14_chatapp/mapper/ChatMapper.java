129243ce49e03b14ddf711cdad7279505002307b
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-07-20 05:19:36 -0500

    expanded mapping functionality via the chatMapper to support messaging feature

--- src/main/java/com/wonderwebdev/a14_chatapp/mapper/ChatMapper.java
+++ src/main/java/com/wonderwebdev/a14_chatapp/mapper/ChatMapper.java
@@ -6,6 +6,7 @@ import org.mapstruct.factory.Mappers;
 
 import com.wonderwebdev.a14_chatapp.domain.Chat;
 import com.wonderwebdev.a14_chatapp.dto.ChatDTO;
+import com.wonderwebdev.a14_chatapp.dto.ChatMessageDTO;
 import com.wonderwebdev.a14_chatapp.dto.ChatSummaryDTO;
 
 @Mapper(componentModel = "spring")
@@ -24,4 +25,23 @@ public interface ChatMapper {
     @Mapping(target = "message", source = "message")
     @Mapping(target = "publishedAt", source = "publishedAt")
     ChatSummaryDTO toSummaryDto(Chat chat);
+
+    @Mapping(target = "channel", source = "channel")
+    @Mapping(target = "user", source = "user")
+    @Mapping(target = "userName", source = "user.userName")
+    ChatMessageDTO toMessageDto(Chat chat);
+
+    @Mapping(target = "channel.description", ignore = true)
+    @Mapping(target = "channel.messages", ignore = true)
+    @Mapping(target = "channel.users", ignore = true)
+    @Mapping(target = "user.channels", ignore = true)
+    @Mapping(target = "user.password", ignore = true)
+    @Mapping(target = "channel", source = "channel")
+    @Mapping(target = "user", source = "user")
+    Chat toEntity(ChatMessageDTO chatMessageDTO);
+
+    @Mapping(target = "userName", source = "user.userName")
+    @Mapping(target = "message", source = "message")
+    @Mapping(target = "publishedAt", source = "publishedAt")
+    ChatSummaryDTO messageDtoToSummaryDto(ChatMessageDTO chatMessageDTO);
 }
